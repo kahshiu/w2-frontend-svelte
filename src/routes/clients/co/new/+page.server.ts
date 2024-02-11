87720a86bc9ba4c5f67a023a00ecc21e95e125c8
kahshiu-ipp <kahshiu.chong@iproperty.com.my>
2024-02-11 18:16:01 +0800

    new company client page

---
+++ src/routes/clients/co/new/+page.server.ts
@@ -0,0 +1,17 @@
+import { fetchDefinition, fetchJson, LIST_CO_CLIENT } from "$lib/shared/ajax";
+import type { ClientCoDto } from "$lib/shared/dto/ProfileDto";
+import { error } from "@sveltejs/kit";
+
+export const load = async () => {
+  const defResult = await fetchDefinition()
+
+  const respClients = await fetchJson<ClientCoDto[]>(LIST_CO_CLIENT);
+  if (respClients.result === null) {
+    error(404, { message: respClients.message })
+  }
+
+  return {
+    definitions: defResult,
+    clients: respClients.result,
+  }
+}
---
+++ src/routes/clients/co/new/+page.svelte
@@ -0,0 +1,29 @@
+<script>
+	import NavMain from '$lib/components/NavMain.svelte';
+	import ClientCoForm from '$lib/components/ClientCoForm.svelte';
+	export let data;
+</script>
+
+<div>
+	<NavMain />
+</div>
+
+<main>
+	<nav class="breadcrumb">
+		<ul>
+			<li><a href="/clients">List of Company Clients</a></li>
+			<li>New Company Client [ID: to be generated]</li>
+		</ul>
+	</nav>
+
+	<h2>New Company Client</h2>
+
+	<form method="POST" action="/clients/co/0?/save">
+		<ClientCoForm
+			definitions={data.definitions}
+		/>
+		<div class="form-actions">
+			<input type="submit" value="Save" />
+		</div>
+	</form>
+</main>
